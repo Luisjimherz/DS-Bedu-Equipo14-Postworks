768e5d7410f232e00b3cd28021de7671bb318510
Mario <mario.alan.16180@gmail.com>
2021-02-01 22:37:33 -0600

    Update Solution_Postwork_S01.R

--- Mario/Solution_Postwork_S01.R
+++ Mario/Solution_Postwork_S01.R
@@ -1,17 +1,12 @@
-football <- read.csv("C:/Users/acer/Documents/TEC/KAUST Learning/BEDU/Modulo 2/Programacion-con-R-Santander-master/Sesion-01/Postwork/SP1.csv")
+football <- read.csv("https://www.football-data.co.uk/mmz4281/1920/SP1.csv")
 
 library(dplyr)
 
 goals <- select(football, FTHG, FTAG)
 
-# count(subset(goals, select = FTHG))
-
 hteam <- table(goals$FTHG)/dim(goals)[1]*100
 vteam <- table(goals$FTAG)/dim(goals)[1]*100
 
-hteam.m <- as.matrix(hteam)
-vteam.m <- as.matrix(vteam)
-
-p.conjunta <- hteam.m %*% t(vteam.m) / 100
+p.conjunta <- table(goals$FTHG,goals$FTAG)/dim(goals)[1]
 
 sum(hteam.m %*% t(vteam.m) / 100)
