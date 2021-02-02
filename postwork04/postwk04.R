e0d6de031339d6f004f4e2108250b74ccfe75189
Luis Fernando Jiménez <66329525+Luisjimherz@users.noreply.github.com>
2021-02-01 21:27:43 -0600

    Update postwk04.R

--- postwork04/postwk04.R
+++ postwork04/postwk04.R
@@ -2,7 +2,7 @@
 # @Equipo: 14 
 
 
-setwd("../postwork02")
+setwd("postwork02")
 df <- read.csv("postwork02.csv")
 names(df)
 View(df)
@@ -36,12 +36,13 @@ FTAG <- df$FTAG
 #    de la cual vienen los cocientes en la tabla anterior. 
 #    Menciona en cuáles casos le parece razonable suponer que los cocientes de la tabla
 #    en el punto 1, son iguales a 1 (en tal caso tendríamos independencia de las variables 
-#aleatorias X y Y).
+#    aleatorias X y Y).
 setwd("../postwork04")
 library(reshape2)
 library(ggplot2)
-(quotients.df <- melt(quotients.tbl))  # omitiendo valores con ceros
-(quotients.df <- quotients.df[quotients.df$value != 0,])
+quotients.tbl[1:5, 1:6]
+(quotients.df <- melt(quotients.tbl[1:5, 1:6]))  
+(quotients.df <- quotients.df[quotients.df$value != 0,]) # omitiendo valores con ceros
 
 ggplot(quotients.df, aes(value)) +
   geom_histogram(binwidth=0.2) +
@@ -52,5 +53,37 @@ ggplot(quotients.df, aes(value)) +
 ggsave("histograma.png")
 
 (original.sample <- quotients.df$value)
+original.sample <- as.data.frame(original.sample)
 
 
+#(resample <- sample(original.sample, length(original.sample), replace=T))
+#(resample <- data.frame(quotients=resample))
+
+#ggplot(resample, aes(quotients)) +
+#  geom_histogram(binwidth=0.2) +
+#  ggtitle("Histograma de cocientes") +
+#  ylab("Frecuencia") +
+#  xlab("") + 
+#  theme_light()
+#ggsave("histograma_B2.png")
+
+#install.packages('boot',dep=TRUE)
+library(boot)
+set.seed(89)
+
+qmed <- function(data, indices){
+  dt <- data[indices, ]
+  return(median(dt))
+}
+bootstrap <- boot(original.sample, statistic=qmed, R=1000)
+head(as.data.frame(bootstrap$t))
+bootstrap
+summary(bootstrap)
+boot.ci(bootstrap, index=1)
+plot(bootstrap)
+ggplot(as.data.frame(bootstrap$t), aes(V1)) +
+  geom_histogram(binwidth=0.009) +
+  ggtitle("Histograma de cocientes") +
+  ylab("Frecuencia") +
+  xlab("") + 
+  theme_light()
